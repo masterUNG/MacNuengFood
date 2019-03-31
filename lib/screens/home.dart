25216810abf84f44e9c4d6b3763d747753770b91
masterUNG <phrombutr@gmail.com>
2019-03-31 09:37:42 +0700

    SnackBar Success

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'register.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -7,6 +9,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // Key
+  final homeFormKey = GlobalKey<FormState>();
+  final homeScaffoldkey = GlobalKey<ScaffoldState>();
+
+  // Explicit
+  String email, password;
+
   // Logo
   Widget showLogo() {
     return Image.asset('images/logo.png');
@@ -31,16 +40,33 @@ class _HomeState extends State<Home> {
           labelStyle: TextStyle(color: Colors.white),
           labelText: 'Email Address :',
           hintText: 'you@email.com'),
+      validator: (String value) {
+        if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'Please Type Email Format';
+        }
+      },
+      onSaved: (String value) {
+        email = value;
+      },
     );
   }
 
   // Password
   Widget passwordTextField() {
     return TextFormField(
+      obscureText: true,
       decoration: InputDecoration(
           labelText: 'Password :',
           hintText: 'more 6 Charator',
           labelStyle: TextStyle(color: Colors.white)),
+      validator: (String value) {
+        if (value.length <= 5) {
+          return 'Please Type Password more 6 Charator';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
     );
   }
 
@@ -52,8 +78,43 @@ class _HomeState extends State<Home> {
         'Sign In',
         style: TextStyle(color: Colors.white),
       ),
-      onPressed: () {},
+      onPressed: () {
+        checkEmailAnPass();
+      },
     );
+  }
+
+  void checkEmailAnPass() async {
+    if (homeFormKey.currentState.validate()) {
+      homeFormKey.currentState.save();
+      print('email = $email, password = $password');
+      String urlString =
+          'https://www.androidthai.in.th/mac/getUserWhereUserMaster.php?isAdd=true&Email=$email';
+
+      var response = await get(urlString);
+      var result = json.decode(response.body);
+      print('result = $result');
+
+      if (result.toString() == 'null') {
+        // User False
+        showSnackBar('No $email in my Database');
+      } else {
+        // User True
+        
+      }
+    }
+  }
+
+  void showSnackBar(String messageString) {
+    final snackBar = new SnackBar(
+      backgroundColor: Colors.red[900],
+      duration: new Duration(seconds: 8),
+      content: Text(messageString),
+      action: new SnackBarAction(
+        label: 'Close', onPressed: (){},
+      ),
+    );
+    homeScaffoldkey.currentState.showSnackBar(snackBar);
   }
 
   // Button SignUp
@@ -76,44 +137,47 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.green[700], Colors.green[50]],
-                begin: Alignment(-1, -1))),
-        padding: EdgeInsets.only(top: 80.0),
-        alignment: Alignment.topCenter,
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              showLogo(),
-              showAppName(),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                child: emailTextField(),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0),
-                child: passwordTextField(),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 15.0),
-                child: Row(
-                  children: <Widget>[
-                    new Expanded(
-                      child: signInButton(),
+        key: homeScaffoldkey,
+        resizeToAvoidBottomPadding: false,
+        body: Form(
+          key: homeFormKey,
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.green[700], Colors.green[50]],
+                    begin: Alignment(-1, -1))),
+            padding: EdgeInsets.only(top: 80.0),
+            alignment: Alignment.topCenter,
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  showLogo(),
+                  showAppName(),
+                  Container(
+                    margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                    child: emailTextField(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                    child: passwordTextField(),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 15.0),
+                    child: Row(
+                      children: <Widget>[
+                        new Expanded(
+                          child: signInButton(),
+                        ),
+                        new Expanded(
+                          child: signUpButton(context),
+                        )
+                      ],
                     ),
-                    new Expanded(
-                      child: signUpButton(context),
-                    )
-                  ],
-                ),
-              )
-            ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
